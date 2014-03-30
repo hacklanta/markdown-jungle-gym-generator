1bfc5daef3fc4625d0897b009865f007ccd81ce6
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2014-03-29 20:00:19 -0400

    Add basics of getting things running.
    This currently includes:
     - Some options (for directory to read, verbosity, sort).
     - Creation/git init of the repository and directory.
     - Reading from the read directory and processing all files into
       a set of FileDiffs.
     - If files described in FileDiffs don’t exist, create them.
    
    We’re not yet creating step commits, and we’re not yet
    dealing with FileDiffs on existing files.
    
    There’s also plenty of sample output still in there that gets
    printed out regardless.

--- generate.rb
+++ generate.rb
@@ -1,8 +1,73 @@
 #!/usr/bin/env ruby
+require './micro_optparse'
+require './file_diff'
 
-puts "Reading directory..."
-strategy = 'alphabetical'
-puts "Using sort strategy #{strategy}"
+sort_strategies = {
+  :alphabetical => proc  { |thing, other_thing| thing <=> other_thing }
+}
+
+options = Parser.new do |parser|
+  parser.banner = <<-BANNER
+    Generate lift-jungle-gym git repositories from a directory of markdown files.
+  BANNER
+
+  parser.version = "markdown-lift-jungle-gym-generator 0.1-alpha"
+  parser.option :directory, "directory to read from", :short => 'd', :default => '.'
+  parser.option :verbose, "enable verbose output"
+  parser.option :sort, "sorting strategy to walk through files in directory", :default => 'alphabetical'
+#  parser.option :chance, "set mutation chance", :default => 0.8, :value_satisfies => lambda {|x| x >= 0.0 && x <= 1.0}
+end.process!
+
+VERBOSE = options[:verbose]
+
+repository_directory = ARGV[0] || 'lift-jungle-gym-repository'
+
+directory = options[:directory]
+sort_strategy = options[:sort] if sort_strategies.include?(options[:sort].to_sym)
+
+unless Dir.exists?(repository_directory)
+  Dir.mkdir(repository_directory)
+end
+
+unless Dir.exists?(File.join(repository_directory, '.git'))
+  Dir.chdir(repository_directory) do
+    if VERBOSE
+      system("git init")
+    else
+      `git init`
+    end
+  end
+end
+
+puts "Reading directory #{directory} using sort strategy #{sort_strategy}..."
+
+absolute_repository_directory = File.realdirpath(repository_directory)
+
+Dir.chdir(directory) do
+  filenames = Dir.entries('.').sort(&sort_strategies[sort_strategy.to_sym])
+
+  filenames.each do |filename|
+    next unless File.file?(filename)
+
+    File.open(filename) do |file|
+      puts "\tProcessing #{filename}" if VERBOSE
+
+      file_diffs = FileDiff.diffs_from(file)
+      puts file_diffs
+
+      Dir.chdir(absolute_repository_directory) do
+        file_diffs.each do |file_diff|
+          if File.exists?(file_diff.filename)
+            file_diff.apply
+          else
+            file_diff.create
+          end
+        end
+      end
+    end
+  end
+end
+
 starting_point = 'git://github.com/Shadowfiend/lift-tutorial.git'
 puts "Using starting point #{starting_point}"
 current_file = "getting-started-section-1"
@@ -12,7 +77,7 @@ puts "\tFound file reference #{file_reference}"
 puts "\tNo eliding found, creating/replacing file"
 puts "\tEliding found, attempting to patch previous file"
 puts "\tCouldn't patch previous file, please select appropriate range"
-puts "\tGenerating checkpoint commit with message #{View-First Development (Lift Getting Started, Section 1)}"
+puts "\tGenerating checkpoint commit with message View-First Development (Lift Getting Started, Section 1)"
 puts "\tTagging section-1"
 puts "Created git history. Repo is ready for push."
 
